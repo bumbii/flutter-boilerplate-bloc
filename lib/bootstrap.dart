dcb3d1a2342f605171337ca6a9581007a65efc9b
phuc <huyphuc0906@gmail.com>
2022-05-23 09:37:48 +0700

    Fix exception when launching on Android

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -6,21 +6,24 @@ import 'package:bumbii/utils/logger.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   FlutterError.onError = (details) {
     AppLogger.instance.e(details.exceptionAsString());
   };
 
-  final storage = await HydratedStorage.build(storageDirectory: kIsWeb
-      ? HydratedStorage.webStorageDirectory : Directory('bumbii'));
+  final Directory appDocDirectory = await getApplicationDocumentsDirectory();
+  final storage = await HydratedStorage.build(
+      storageDirectory: kIsWeb
+          ? HydratedStorage.webStorageDirectory
+          : Directory('${appDocDirectory.path}/bumbii'));
   await runZonedGuarded(
     () async {
-      await HydratedBlocOverrides.runZoned(
-        () async => runApp(await builder()),
-        blocObserver: AppBlocObserver(),
-        storage: storage
-      );
+      await HydratedBlocOverrides.runZoned(() async => runApp(await builder()),
+          blocObserver: AppBlocObserver(), storage: storage);
     },
     (error, stackTrace) => AppLogger.instance.e(error.toString()),
   );
